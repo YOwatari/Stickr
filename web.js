6f78bcd2bc60083f803880c00a8c742a87e1398d
YOwatari <yyutao88@gmail.com>
2014-02-16 20:27:01 +0900

    created web.js

---
+++ web.js
@@ -0,0 +1,10 @@
+var static = require('node-static');
+
+var file = new(static.Server)('./app');
+var port = process.env.PORT || 5000;
+
+require('http').createServer(function (request, response){
+    request.addListener('end', function() {
+        file.serve(request, response);
+    });
+}).listen(port);
