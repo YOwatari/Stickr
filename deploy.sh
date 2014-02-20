e2791f14d684b5b0cb09bf93b480da6cc001f720
YOwatari <yyutao88@gmail.com>
2014-02-20 16:50:36 +0900

    どや？

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+  cd dist
+  git init || true
+  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  git remote add heroku git@heroku.com:stickr.git
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  yes | heroku keys:add
+  yes | git add -A
+  yes | git commit -m "Deployment update" --allow-empty
+  yes | git push heroku master
+  heroku restart
+  heroku keys:remove `whoami`@`hostname`
+  cd ..
+fi
+
+if [ "$TRAVIS_BRANCH" == "deploy" ]
+then
+  cd dist
+  git init || true
+  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+  git remote add heroku git@heroku.com:stickr-staging.git
+  echo "Host heroku.com" >> ~/.ssh/config
+  echo "   StrictHostKeyChecking no" >> ~/.ssh/config
+  echo "   CheckHostIP no" >> ~/.ssh/config
+  echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  yes | heroku keys:add
+  yes | git add -A
+  yes | git commit -m "Deployment update" --allow-empty
+  yes | git push heroku master
+  heroku restart
+  heroku keys:remove `whoami`@`hostname`
+  cd ..
+fi
