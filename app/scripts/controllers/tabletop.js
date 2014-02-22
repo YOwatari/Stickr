0339e2f2eb8ccd203b13c25934c6f9b9f296207b
YOwatari <yyutao88@gmail.com>
2014-02-22 11:43:07 +0900

    各種ページを生成

---
+++ app/scripts/controllers/tabletop.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('stickrApp')
+  .controller('TabletopCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
