89343ca4bce3d36a219c61cf4ce5bfef5e42f459
S.Hatamori <syogogo50@gmail.com>
2014-02-22 19:40:26 +0900

    ハイライト前です

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,9 +68,11 @@ module.exports = function (grunt) {
       livereload: {
         files: [
           '<%= yeoman.app %>/views/{,*//*}*.{html,jade}',
+          '<%= yeoman.app %>/views/{,*//*//*}*.{html,jade}',
           '{.tmp,<%= yeoman.app %>}/styles/{,*//*}*.css',
           '{.tmp,<%= yeoman.app %>}/scripts/{,*//*}*.js',
           '<%= yeoman.app %>/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}',
+          "<%= yeoman.app %>/views/partials/edit/serect_tabletop.html"
         ],
       
         options: {
@@ -281,11 +283,11 @@ module.exports = function (grunt) {
     },
 
     // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/views/*.html']
-      }
-    },
+    //cdnify: {
+    //  dist: {
+    //    html: ['<%= yeoman.dist %>/views/*.html']
+//    }
+    //},
 
     // Copies remaining files to places other tasks can use
     copy: {
