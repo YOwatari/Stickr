10d90a09647ea9f33c8af5277291b441f71f3cf6
S.Hatamori <syogogo50@gmail.com>
2014-02-23 10:44:51 +0900

    マウスオーバー実装

--- app/scripts/controllers/select_tabletop.js
+++ app/scripts/controllers/select_tabletop.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('stickrApp')
-  .controller('EditSeletctTabletopCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+	.controller('EditSeletctTabletopCtrl', function ($scope, $http) {
+		$http.get('/api/awesomeThings').success(function(awesomeThings) {
+		$scope.awesomeThings = awesomeThings;
+			});
+	});
+});
+// angular.module('stickrApp')
+//   .directive("tabletop", function () {
+//     return function (scope, element, attr) {
+//         element.bind('mouseover', function (event) {
+//         	console.log("over");
+// 			//$("#tabletop img").fadeTo(0.1, 0.25);
+// 			//$(this).fadeTo(0.1, 1);
+//         });
+//         element.bind('mouseout', function (event) {
+//         	//$(this).fadeTo(0.1, 1);
+//         	console.log("out");
+//         });
+//     }
+// });
